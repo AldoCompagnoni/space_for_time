820160a00f8841d1e749ac505a53a2070d1ec7b4
Compagnoni <ac22qawo@idiv.de>
2021-01-22 18:44:59 +0100

    Fit climate models

--- R/clim_analysis_ks.R
+++ R/clim_analysis_ks.R
@@ -2,12 +2,14 @@
 library(tidyverse)
 library(testthat)
 library(plotly)
+library(lme4)
 options(stringsAsFactors = F)
 
 # read data 
 ks        <- read.csv('data/adler/kansas/allrecords.csv')
-ks_q      <- read.csv('data/adler/kansas/quadrat_info.csv')
-
+ks_q      <- read.csv('data/adler/kansas/quadrat_info.csv') %>% 
+              dplyr::select(-shapefiles,-quadX,-quadY)
+                                                                          
 # prec/temp anomalies
 prec_anom <- read.csv('data/adler/kansas/cruts_hays_prec_anom.csv')
 temp_anom <- read.csv('data/adler/kansas/cruts_hays_temp_anom.csv')
@@ -85,18 +87,22 @@ p_bogr <- ks_n %>%
                  color = group,
                  group = group) ) 
 
-# explore interactively (explude groups with little BOGR)
+# explore interactively (exclude groups with little BOGR)
 ggplotly(p_bogr)
+
+
 
 # Growth rates by N -------------------------------------
 
 # by plot
 ks_nt1     <- ks_n %>% 
+                left_join( ks_q ) %>% 
                 subset( !(n %in% 0) ) %>% 
                 rename( nt1 = n ) %>% 
                 mutate( yr = as.numeric(yr) )
   
 ks_nt0     <- ks_n %>% 
+                left_join( ks_q ) %>% 
                 subset( !(n %in% 0) ) %>% 
                 rename( nt0 = n ) %>% 
                 mutate( yr = as.numeric(yr) ) %>% 
@@ -104,18 +110,21 @@ ks_nt0     <- ks_n %>%
   
 ks_gr_n    <- left_join( ks_nt0, ks_nt1 ) %>% 
                 mutate( gr = log(nt1 / nt0) ) %>% 
-                subset( !is.na(gr) ) 
+                subset( !is.na(gr) ) %>% 
+                left_join( rename(prec_anom, yr=MatrixEndYear) )
 
 
 # Growth rates by AREA -------------------------------------
 
 # by plot
 ks_at1     <- ks_a %>% 
+                left_join( ks_q ) %>% 
                 subset( !(area %in% 0 | is.na(area) ) ) %>% 
                 rename( at1 = area ) %>% 
                 mutate( yr = as.numeric(yr) )
 
 ks_at0     <- ks_a %>% 
+                left_join( ks_q ) %>% 
                 subset( !(area %in% 0 | is.na(area)) ) %>% 
                 rename( at0 = area ) %>% 
                 mutate( yr = as.numeric(yr) ) %>% 
@@ -123,11 +132,81 @@ ks_at0     <- ks_a %>%
 
 ks_gr_a    <- left_join( ks_at0, ks_at1 ) %>% 
                 mutate( gr = log(at1 / at0) ) %>% 
-                subset( !is.na(gr) ) 
+                subset( !is.na(gr) ) %>% 
+                left_join( rename(prec_anom, yr=MatrixEndYear) )
 
 
 # Only analyze bouteoula gracilis ----------------------------------------
 
 # Ideal functional form 
-# gr ~ ppt_t0|group + nt0
-# gr ~ ppt_t0|group + at0
+# gr ~ ppt_t0|group + nt0 versus gr ~ ppt_t0|group
+# gr ~ ppt_t0|group + at0 versus gr ~ ppt_t0|group
+
+# also, could we pick 
+
+# correlations are strong
+cor_mat <- ks_gr_n %>%
+  subset( species == 'Bouteloua gracilis' ) %>%
+  group_by( yr, group ) %>% 
+  summarise( gr = mean(gr,na.rm=T) ) %>% 
+  ungroup %>% 
+  # MOST ABUNDANT groups. I've picked these out using ggplotly
+  subset( group %in% c('et1', 'et2', 'et3', 'lb2', 'lb3',
+                       'sg1', 'sg1a', 'sg2', 'sg3') ) %>% 
+  pivot_wider( names_from = group,
+               values_from = gr ) %>% 
+  dplyr::select( -yr) %>% 
+  as.matrix %>% 
+  cor(use='na.or.complete') 
+  
+
+cor_mat[upper.tri(cor_mat)] %>% as.numeric %>% hist
+
+  # subset( n != 0 ) %>%
+  ggplot() +
+  geom_line( aes(x=yr,y=gr,
+                 color = group,
+                 group = group) ) 
+
+# estimate climatic effects  
+mod <- lmer( gr ~ at0 + ppt_t0 + (ppt_t0|group) , data= ks_gr_a) 
+mod <- lmer( gr ~ ppt_t0 + (0 + ppt_t0|group), data= ks_gr_a)
+
+mod <- lmer( gr ~ nt0 + ppt_t0 + (ppt_t0|group) , data= ks_gr_n) 
+mod <- lmer( gr ~ ppt_t0 + (ppt_t0|group) , data= ks_gr_n) 
+
+ks_gr_a_group <- ks_gr_a %>% 
+  group_by(yr,group) %>% 
+  summarise( gr = mean(gr,na.rm=T) ) %>% 
+  ungroup  %>% 
+  left_join( rename(prec_anom, yr=MatrixEndYear) )
+
+ks_gr_n_group <- ks_gr_n %>% 
+  group_by(yr,group) %>% 
+  summarise( gr = mean(gr,na.rm=T) ) %>% 
+  ungroup  %>% 
+  left_join( rename(prec_anom, yr=MatrixEndYear) )
+
+ggplot(ks_gr_a) +
+  geom_point( aes(ppt_t0, gr) )
+
+
+# estimate climatic effects 10 years at a time -------------------
+
+# 
+yr_v <- ks_gr_a_group$yr %>% unique %>% sort
+
+# fit linear model to a subset of years
+lm_subset <- function( ii ){
+  
+  lm(gr ~ ppt_t0,
+     data=subset(ks_gr_a, yr %in% yr_v[(c(1:10)+(ii-1))]) ) %>% 
+    coef %>% 
+    .[2]
+   
+}
+
+# retrieve betas
+betas <- sapply(1:29, lm_subset)
+
+
